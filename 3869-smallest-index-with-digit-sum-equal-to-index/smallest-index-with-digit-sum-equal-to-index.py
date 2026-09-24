d1dc611e0dc7fc052ed957961c72c6c01aa7906d
parthgp2009 <parthgp20@gmail.com>
2026-09-24 22:58:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.07%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            n=nums[i]
+            s=0
+            while n>0:
+                s+=n%10
+                n=n//10
+            if s==i:
+                return i
+                break
+        return -1
